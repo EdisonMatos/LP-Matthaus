f6b2cf8c1c25b27288cfc14534024fe3ec1eaec9
Edison Matos <edison.matos@live.com>
2025-01-10 14:57:39 -0300

    feat: Desativa rota lp01

--- src/main.jsx
+++ src/main.jsx
@@ -13,7 +13,7 @@ ReactDOM.createRoot(document.getElementById("root")).render(
   <Router>
     <Routes>
       <Route path="/" element={<Index />} />
-      <Route path="/lp01" element={<Lp01 />} />
+      {/* <Route path="/lp01" element={<Lp01 />} /> */}
       <Route path="/bpc-loas" element={<Lp02 />} />
       <Route path="/penal" element={<Lp03 />} />
     </Routes>
